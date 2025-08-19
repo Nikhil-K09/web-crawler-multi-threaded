987e747cce66f2ea1f4a4b29d710330912368c63
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-08-19 12:41:42 +0530

    URL Fetcher

---
+++ src/main/java/org/example/URLFetcher.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class URLFetcher {
+    public static Set<String> fetchLinks(String url){
+        Set<String> links=new HashSet<>();
+        Document document=null;
+        try {
+            document= Jsoup.connect(url).timeout(50000).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.print(document.text());
+
+        Elements anchorTags=document.select("a[href]");
+        for(Element link : anchorTags){
+            String extractedUrl=link.absUrl("href");
+            if(!extractedUrl.isEmpty()){
+                links.add(extractedUrl);
+                System.out.print(links);
+            }
+        }
+        return links;
+    }
+}
